62f1a839638902c6dd300365104d04ae84f5bfda
derek089 <nguyen.hai.dang@icloud.com>
2023-01-07 08:58:15 +0700

    1st commit

---
+++ ToDo.BlazorApp/Data/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.SqlTypes;
+
+namespace ToDo.BlazorApp.Data;
+
+public class ToDoItem
+{
+    public string Title { get; set; }
+    public bool Done { get; set; }
+    public string Note { get; set; }
+}
---
+++ ToDo.BlazorApp/Data/ToDoItemService.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace ToDo.BlazorApp.Data;
+
+public class ToDoItemService
+{
+    private static List<ToDoItem> _data = new List<ToDoItem>()
+    {
+        new ToDoItem { Title = "Meet Donald Trump" },
+        new ToDoItem { Title = "Lunch with Barrack Obama" },
+        new ToDoItem { Title = "Go fishing with Bill Gate" },
+        new ToDoItem { Title = "Walking with Putin" }
+    };
+
+    private readonly string _file = "Data\\todo.json";
+
+    public List<ToDoItem> GetData()
+    {
+        return _data;
+    }
+
+    public void SaveChange()
+    {
+        string serialize = JsonSerializer.Serialize<List<ToDoItem>>(_data);
+        File.WriteAllText(_file,serialize);
+    }
+}
